3229e08cfbc53b149fabd71cf538acdb577d3a67
msbrandt00 <msbrandt00@gmail.com>
2023-06-04 11:39:57 -0600

    clean up unused code

--- infrastructure/stack/journal_stack.py
+++ infrastructure/stack/journal_stack.py
@@ -7,7 +7,6 @@ from aws_cdk import (
     aws_ecr as ecr,
     aws_lambda as _lambda,
     aws_iam as iam,
-    # aws_lambda_python_alpha as lpa,
     BundlingOptions as bo,
     aws_events as events,
     aws_events_targets as targets,
@@ -34,25 +33,9 @@ class JournalStack(Stack):
 
         data_sources = ['lastFm', 'strava']
         lambdas = []
+
+        # ingest lambdas
         for data_source in data_sources:
-
-            # lambda definition
-            layer_name = generateResourceName(f"lambda-layer-{data_source}")
-
-            # lambda_layer = lpa.PythonLayerVersion(
-            #     self,
-            #     layer_name,
-            #     layer_version_name=layer_name,
-            #     entry=f'../data-pipelines/{data_source}Pipeline/lambdas/{data_source}_layer/'
-            # )
-            # lambdaLayer = _lambda.LayerVersion(
-            #     self,
-            #     layer_name,
-            #     code=_lambda.AssetCode(
-            #         f'../data-pipelines/{data_source}Pipeline/lambdas/{data_source}_layer/'),
-            #     compatible_runtimes=[
-            #         _lambda.Runtime.PYTHON_3_9],
-            # )
 
             lambda_function_name = generateResourceName(
                 f"ingest-{data_source}")
@@ -70,30 +53,15 @@ class JournalStack(Stack):
                         command=[
                             "bash", "-c",
                             "pip install --no-cache -r requirements.txt -t /asset-output && cp -au . /asset-output",
-                            "cp -au "
+                            # "cp -au "
                         ],
                     ),
                 ),
                 # role=lambda_role,
-                # layers=[lambdaLayer],
                 environment={}
-            )            # lambda_function_name = generateResourceName(
-        #     f"ingest-{data_source}")
-
-        # lambda_function = lpa.PythonFunction(
-        #     self,
-        #     lambda_function_name,
-        #     runtime=_lambda.Runtime.PYTHON_3_7,
-        #     timeout=Duration.seconds(900),
-        #     memory_size=256,
-        #     # environment = {},
-        #     entry=f'../data-pipelines/{data_source}Pipeline/lambdas/{data_source}_ingest/',
-        #     index='lambda_function.py',
-        #     handler='lambda_handler',
-        #     layers=[lambda_layer]
-        # )
-
+            )
             lambdas.append(cdk_lambda)
+
         parallel_execution = sfn.Parallel(self, 'ParallelIngest')
         for lambda_function in lambdas:
             parallel_execution.branch(sfn_tasks.LambdaInvoke(
@@ -103,16 +71,11 @@ class JournalStack(Stack):
             )
 
         # state machine
-        # definition = parallel_execution.build()
         state_machine_name = generateResourceName('state-machine')
-
-        # state_machine = sfn.StateMachine(
         sfn.StateMachine(
             self,
             state_machine_name,
-
             definition=parallel_execution,
-
             state_machine_name=state_machine_name
         )
 
