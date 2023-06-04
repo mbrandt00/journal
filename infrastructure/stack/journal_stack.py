da9b11a65a91b9bd1681a4c2b5ff501dbc90daf3
msbrandt00 <msbrandt00@gmail.com>
2023-06-04 00:31:34 -0600

    lambda layer in lpa requires python 3.7

--- infrastructure/stack/journal_stack.py
+++ infrastructure/stack/journal_stack.py
@@ -51,7 +51,7 @@ class JournalStack(Stack):
             lambda_function = lpa.PythonFunction(
                 self,
                 lambda_function_name,
-                runtime=_lambda.Runtime.PYTHON_3_8,
+                runtime=_lambda.Runtime.PYTHON_3_7,
                 timeout=Duration.seconds(900),
                 memory_size=256,
                 # environment = {},
