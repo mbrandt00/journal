af2ee414b6b93740c0a7b17ca567f32b3f955136
msbrandt00 <msbrandt00@gmail.com>
2023-06-05 23:47:01 -0600

    retry

--- data-pipelines/lastFmPipeline/lambdas/lastFm_ingest/layer/api.py
+++ data-pipelines/lastFmPipeline/lambdas/lastFm_ingest/layer/api.py
@@ -15,7 +15,7 @@ def get_secret():
 
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-    except ClientError as e:
+    except client.exceptions.ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
