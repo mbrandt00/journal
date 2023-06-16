8f1973e45ddb02230d4be881d761d1c9b2aa1c0c
msbrandt00 <msbrandt00@gmail.com>
2023-06-15 19:06:27 -0600

    Ingest lambda handles secret rotation instead of separate lambda

--- dataPipelines/stravaPipeline/lambdas/strava_ingest/lambda_function.py
+++ dataPipelines/stravaPipeline/lambdas/strava_ingest/lambda_function.py
@@ -1,56 +1,37 @@
-
 import os
 from stravalib.client import Client
-from layer.api import access_token
+from layer.api import get_current_access_token
 from layer.s3 import upload_object
-from layer.utils import dicts_to_jsonl
+from layer.utils import dicts_to_jsonl, date_decider
 import datetime as dt
-from datetime import timedelta
-from layer.activity import Activity
+
+from layer.activity import create_activity
+
 
 today = dt.datetime.today()
-
 date_path = f"year={today.year}/month={today.strftime('%m')}/day={today.strftime('%d')}"
 
 
-
 def lambda_handler(event, context):
-    if event["time_to"] == "today":
-        before = dt.datetime.today()
-    else:
-        before = dt.datetime.strptime(event["time_to"], "%m-%d-%Y")
-
-    if event["time_from"] == "yesterday":
-        after = dt.datetime.today() - timedelta(days=1)
-    else:
-        after = dt.datetime.strptime(event["time_from"], "%m-%d-%Y")
-    client = Client(access_token=access_token)
+    before, after = date_decider(event["time_from"], event["time_to"])
+    client = Client(access_token=get_current_access_token())  # updates token if expired
     activities = client.get_activities(
-        after=after,
         before=before,
+        after=after,
     )
 
     all_activities = []
-    for activity in activities:
-        serialized_activity = Activity(
-            average_heartrate=activity.average_heartrate,
-            average_speed=activity.average_speed,
-            name=activity.name,
-            start_date_local=activity.start_date_local,
-            start_latlng=activity.start_latlng,
-            end_latlng=activity.end_latlng,
-            pr_count=activity.pr_count,
-            description=activity.description,
-            commute=activity.commute,
-            distance=activity.distance,
-            elapsed_time=activity.elapsed_time,
-        ).serialize_activity()
-        all_activities.append(serialized_activity)
+
+    try:
+        for activity in activities:
+            all_activities.append(create_activity(activity))
+    except Exception as e:
+        print(f"exception: {e}")
 
     if all_activities:
         filename = f"strava_raw_{today.strftime('%H:%M')}.json"
         s3_loc = os.path.join("strava", date_path, filename)
-        print(type(all_activities))
-        print(all_activities)
         json_str = dicts_to_jsonl(all_activities)
         upload_object(os.environ["RAW_BUCKET"], s3_loc, json_str, is_string=False)
+    else:
+        print("no activities found")
