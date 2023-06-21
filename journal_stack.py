302524247f14e96fd6e5c9d313af2653cc3ca355
msbrandt00 <msbrandt00@gmail.com>
2023-06-20 23:48:09 -0600

    Fix react hosting

--- journal_stack.py
+++ journal_stack.py
@@ -4,18 +4,23 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_ecr as ecr,
+    aws_iam as iam,
     aws_glue as glue,
     aws_ec2 as ec2,
     aws_rds as rds,
     aws_cloudfront as cloudfront,
     aws_route53 as route53,
     aws_s3 as s3,
+    aws_ec2 as ec2,
     aws_route53_targets as route53_targets,
     aws_certificatemanager as acm,
     aws_ssm as ssm,
     aws_s3_deployment as s3_deployment,
     aws_secretsmanager as secretsmanager,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
     RemovalPolicy,
+    SecretValue,
     Duration,
 )
 from constructs import Construct
@@ -41,24 +46,24 @@ class JournalStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # ----------frontend---------------
+
         apex_domain = os.getenv("JOURNAL_DOMAIN")
-        # react resources
-        react_domain = apex_domain
-        react_hosted_zone = route53.HostedZone(
+        hosted_zone = route53.HostedZone.from_lookup(
             self,
-            "HostedZone",
-            zone_name=apex_domain,
-            comment="Zone for Front End React",
+            generateResourceName("chronolog-hosted-zone"),
+            domain_name=f"{apex_domain}.",
         )
-        react_certificate = acm.Certificate(
+
+        certificate = acm.Certificate(
             self,
-            generateResourceName("react-certificate"),
+            generateResourceName("certificate"),
             domain_name=apex_domain,
-            certificate_name=generateResourceName(
-                "react-certificate"
-            ),  # Optionally provide an certificate name
-            validation=acm.CertificateValidation.from_dns(react_hosted_zone),
+            certificate_name=generateResourceName("certificate"),
+            validation=acm.CertificateValidation.from_dns(hosted_zone),
         )
+
+        # Origin Access Identity
         react_bucket = s3.Bucket(
             self,
             generateResourceName("react-bucket"),
@@ -69,7 +74,6 @@ class JournalStack(Stack):
             website_index_document="index.html",
         )
 
-        # Origin Access Identity
         react_oai = cloudfront.OriginAccessIdentity(
             self,
             generateResourceName("react-oai"),
@@ -117,29 +121,9 @@ class JournalStack(Stack):
                 ),
             ],
             viewer_certificate=cloudfront.ViewerCertificate.from_acm_certificate(
-                certificate=react_certificate,
-                aliases=[react_domain],
+                certificate=certificate,
+                aliases=[apex_domain],
             ),
-        )
-
-        # Route53 alias record for CloudFront Distribution
-        route53.ARecord(
-            self,
-            generateResourceName("react-alias-record"),
-            record_name=react_domain,
-            target=route53.RecordTarget.from_alias(
-                route53_targets.CloudFrontTarget(react_distribution)
-            ),
-            zone=react_hosted_zone,
-        )
-        route53.ARecord(
-            self,
-            generateResourceName("react-www-record"),
-            record_name=f"www.{react_domain}",
-            target=route53.RecordTarget.from_alias(
-                route53_targets.CloudFrontTarget(react_distribution)
-            ),
-            zone=react_hosted_zone,
         )
 
         # deploy react front end
@@ -151,3 +135,100 @@ class JournalStack(Stack):
             distribution=react_distribution,
             distribution_paths=["/*"],
         )
+
+        # Route53 alias record for CloudFront Distribution
+        route53.ARecord(
+            self,
+            generateResourceName("react-alias-record"),
+            record_name=apex_domain,
+            target=route53.RecordTarget.from_alias(
+                route53_targets.CloudFrontTarget(react_distribution)
+            ),
+            zone=hosted_zone,
+        )
+        # route53.ARecord(
+        #     self,
+        #     generateResourceName("react-www-record"),
+        #     record_name=,
+        #     target=route53.RecordTarget.from_alias(
+        #         route53_targets.CloudFrontTarget(react_distribution)
+        #     ),
+        #     zone=hosted_zone,
+        # )
+
+        # ----------backend---------------
+
+        # vpc = ec2.Vpc(
+        #     self,
+        #     generateResourceName("journal-vpc"),
+        #     ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
+        #     max_azs=2,
+        #     nat_gateways=1,
+        # )  # set cidr range?
+
+        # repo = ecr.Repository.from_repository_name(
+        #     self, generateResourceName("ecr"), repository_name=os.getenv("ECR_REPO")
+        # )
+        # apex_domain = os.getenv("RAILS_DOMAIN")
+        # # rails_hosted_zone = route53.HostedZone(
+        # #     self,
+        # #     generateResourceName("rails-hosted-zone"),
+        # #     zone_name=apex_domain,
+        # #     comment="Zone for Backend",
+        # # )
+        # rails_domain = apex_domain
+
+        # # Rails API IAM User
+        # rails_iam_user_name = generateResourceName("rails-iam-user")
+        # rails_iam_user = iam.User(
+        #     self, rails_iam_user_name, user_name=rails_iam_user_name
+        # )
+
+        # # Create Access Key and store in Secrets Manager
+        # rails_iam_user_access_key = iam.AccessKey(
+        #     self, f"{rails_iam_user_name}-access-key", user=rails_iam_user
+        # )
+        # rails_iam_user_access_key_secret = secretsmanager.Secret(
+        #     self,
+        #     f"{rails_iam_user_name}-secret",
+        #     secret_name=f"{rails_iam_user_name}-secret",
+        #     secret_object_value={
+        #         "AccessKey": SecretValue.unsafe_plain_text(
+        #             rails_iam_user_access_key.access_key_id
+        #         ),
+        #         "SecretAccessKey": rails_iam_user_access_key.secret_access_key,
+        #     },
+        # )
+
+        # # fargate service
+
+        # task_definition = ecs.FargateTaskDefinition(
+        #     self,
+        #     generateResourceName("Task-Definition"),
+        #     cpu=256,
+        #     memory_limit_mib=512,
+        # )
+
+        # task_definition.add_container(
+        #     "web",
+        #     image=ecs.ContainerImage.from_ecr_repository(repo),
+        #     logging=ecs.LogDrivers.aws_logs(stream_prefix="web_container"),
+        #     entry_point=["./docker-scripts/docker-entrypoint.sh"],
+        #     container_name="web",
+        #     cpu=256,
+        #     memory_limit_mib=512,
+        #     port_mappings=[ecs.PortMapping(container_port=80)],
+        # )
+        # fargate = ecs_patterns.ApplicationLoadBalancedFargateService(
+        #     self,
+        #     generateResourceName("ecs-fargateService"),
+        #     vpc=vpc,
+        #     domain_name=rails_domain,
+        #     domain_zone=hosted_zone,
+        #     certificate=certificate,
+        #     health_check_grace_period=Duration.seconds(150),
+        #     enable_execute_command=True,
+        #     cpu=256,
+        #     memory_limit_mib=512,
+        #     task_definition=task_definition,
+        # )
